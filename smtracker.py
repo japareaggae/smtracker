073df5826c428280caf2ae30f0a6bc04d73283ca
Renan Guilherme <japareaggae@gmail.com>
2016-02-02 09:50:19 -0400

    Initial argparse implementation

--- smtracker.py
+++ smtracker.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python3
+import argparse
 import os
 import xml.etree.ElementTree as etree
 
 DIFFICULTIES = ["Beginner", "Easy", "Medium", "Hard", "Challenge"]
-DEBUG = False
 
-# TODO: Implement argparse to read a specific Stats.xml file
+# Sets the default location for statsxml.
 # TODO: Try to read the MachineProfile if there's no LocalProfile
 # TODO: See if this works properly on both Cygwin and cmd.exe
 if os.name == 'posix':
-    statsxml = os.environ['HOME'] + "/.stepmania-5.0/Save/LocalProfiles/00000000/Stats.xml"
+    def_statsxml = os.environ['HOME'] + "/.stepmania-5.0/Save/LocalProfiles/00000000/Stats.xml"
 elif os.name == 'nt':
-    statsxml = os.environ['APPDATA'] + "/StepMania 5/Save/LocalProfiles/00000000/Stats.xml"
+    def_statsxml = os.environ['APPDATA'] + "/StepMania 5/Save/LocalProfiles/00000000/Stats.xml"
 
-if DEBUG:
-    print("DEBUG: Stats.xml file is " + statsxml)
+# We can ask the program to read a specific file using argparse.
+parser = argparse.ArgumentParser(description='A StepMania Score Tracker')
+parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default=def_statsxml,
+        help="the Stats.xml file to read (will read from first available StepMania profile if not specified)")
+args = parser.parse_args()
+statsxml = vars(args)['file']
 
 try:
     tree = etree.parse(statsxml)
