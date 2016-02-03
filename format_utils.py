57fce7474ac878d3c3e0488c1c7b7ac6c01dabf3
Renan Guilherme <japareaggae@gmail.com>
2016-02-03 19:25:30 -0400

    format_utils: PEP8-ify

--- format_utils.py
+++ format_utils.py
@@ -1,8 +1,12 @@
+#!/usr/bin/env python3
+
+"""Formatting utilities used by outputs."""
+
 
 def highscore_stat(step, stat):
-    """Receives a <Steps> ElementTree, and returns the specified stat from it's first HighScore.
+    """Receives a <Steps> ElementTree and returns a stat from it's first HighScore.
 
-    May raise a AttributeError if a song has no <HighScore> (raised from ElementTree)
+    Will raise an AttributeError if a song has no <HighScore>.
     """
     return step.find("HighScoreList").find("HighScore").find(stat).text
 
@@ -25,36 +29,36 @@ def highscore_timings(step):
 
 
 def tier_to_grade_sm5(tier):
-    """Receives a tier, and converts it to a grade according to SM5 default metrics."""
+    """Converts a tier to a grade according to SM5 default metrics."""
     grades = {'Failed': 'F',
-            'Tier07': 'D',
-            'Tier06': 'C',
-            'Tier05': 'B',
-            'Tier04': 'A',
-            'Tier03': 'AA',
-            'Tier02': 'AAA',
-            'Tier01': 'AAAA'}
+              'Tier07': 'D',
+              'Tier06': 'C',
+              'Tier05': 'B',
+              'Tier04': 'A',
+              'Tier03': 'AA',
+              'Tier02': 'AAA',
+              'Tier01': 'AAAA'}
     return grades[tier]
+
 
 def tier_to_grade_itg(tier):
-    """Receives a tier, and converts it to a grade according to ITG/Simply Love metrics."""
+    """Converts a tier to a grade according to ITG/Simply Love metrics."""
     grades = {'Failed': 'F',
-            'Tier17': 'D',
-            'Tier16': 'C-',
-            'Tier15': 'C',
-            'Tier14': 'C+',
-            'Tier13': 'B-',
-            'Tier12': 'B',
-            'Tier11': 'B+',
-            'Tier10': 'A-',
-            'Tier09': 'A',
-            'Tier08': 'A+',
-            'Tier07': 'S-',
-            'Tier06': 'S',
-            'Tier05': 'S+',
-            'Tier04': '★',
-            'Tier03': '★★',
-            'Tier02': '★★★',
-            'Tier01': '★★★★'}
+              'Tier17': 'D',
+              'Tier16': 'C-',
+              'Tier15': 'C',
+              'Tier14': 'C+',
+              'Tier13': 'B-',
+              'Tier12': 'B',
+              'Tier11': 'B+',
+              'Tier10': 'A-',
+              'Tier09': 'A',
+              'Tier08': 'A+',
+              'Tier07': 'S-',
+              'Tier06': 'S',
+              'Tier05': 'S+',
+              'Tier04': '★',
+              'Tier03': '★★',
+              'Tier02': '★★★',
+              'Tier01': '★★★★'}
     return grades[tier]
-
