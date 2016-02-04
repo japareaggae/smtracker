a4188d61865bb9b9b8d30b8031c2ee9eb687dfa1
Renan Guilherme <japareaggae@gmail.com>
2016-02-04 19:18:13 -0400

    output/plain: Import missing utils.parse

--- output/plain.py
+++ output/plain.py
@@ -1,6 +1,7 @@
 import xml.etree.ElementTree as etree
 
 import utils.format
+import utils.parse
 
 
 def report(stats_xml, mode, difficulties):
@@ -28,8 +29,8 @@ def report(stats_xml, mode, difficulties):
             try:
                 if Song[step_counter].attrib['Difficulty'] == diff and Song[step_counter].attrib['StepsType'] == mode:
                     try:
-                        grade = utils.format.tier_to_grade_sm5(utils.format.highscore_stat(Song[step_counter], "Grade"))
-                        percent = float(utils.format.highscore_stat(Song[step_counter], "PercentDP")) * 100
+                        grade = utils.format.tier_to_grade_sm5(utils.parse.highscore_stat(Song[step_counter], "Grade"))
+                        percent = float(utils.parse.highscore_stat(Song[step_counter], "PercentDP")) * 100
                         print('+++ {:10}: {:3} ({:.2f})'.format(diff, grade, percent))
                     except AttributeError:
                         print("--- " + diff)
