8210bfd8abff56495d4ec5873163a5c67ba8ddb2
Renan Guilherme <japareaggae@gmail.com>
2016-02-14 21:31:15 -0400

    Release 1.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='smtracker',
-    version='1.0.1',
+    version='1.1.0',
     description='A StepMania score tracker',
     long_description=long_description,
     url='https://github.com/japareaggae/smtracker-python',
